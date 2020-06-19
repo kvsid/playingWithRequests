ba3ccbb8d007b45cebc50c95900cfa2981596944
kvsid <nice.curly2013@gmail.com>
2020-06-19 15:44:50 +0300

    Base functional

--- playingWithRequests/ViewController.swift
+++ playingWithRequests/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageContainer: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let imageURL = URL(string: "https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA05982_hires.jpg")!
+        
+        let task = URLSession.shared.dataTask(with: imageURL) {
+            (data, response, error) in
+            
+            if error == nil {
+                let loadedImage = UIImage(data: data!)
+                self.imageContainer.image = loadedImage
+            }
+        }
+        task.resume()
     }
 
 
