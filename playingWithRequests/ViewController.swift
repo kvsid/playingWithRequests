37801bd8e923f26dfefaf63fdd86c7b3335ea717
kvsid <nice.curly2013@gmail.com>
2020-06-19 15:20:43 +0300

    Initial Commit

---
+++ playingWithRequests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playingWithRequests
+//
+//  Created by Xenia Sidorova on 19.06.2020.
+//  Copyright © 2020 Xenia Sidorova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
